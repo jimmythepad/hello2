f55a6c36cf0d64617cdafdcccc580ca005bba225
Jimmy Moore <jimmythepad@gmail.com>
2018-05-27 21:00:17 +0800

    Wod Commit

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello Wold!</title>
+	<title>Hello Wod!</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
